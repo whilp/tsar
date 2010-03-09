ba1a03fdf2a2e73f5921d782a6b87296a11fa769
Will Maier <willmaier@ml1.net>
2010-03-09 16:44:51 -0600

    Add some options.

--- lib/tsar/web.py
+++ lib/tsar/web.py
@@ -5,6 +5,34 @@ from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler
 from tornado.wsgi import WSGIApplication
 
+class DSNType(object):
+    
+    def __init__(self, host="localhost", port=0, db=0, sep=':'):
+        self.host = host
+        self.port = port
+        self.db = db
+        self.sep = sep
+
+    def __str__(self):
+        return "%s:%d/%d" % (self.host, self.port, self.db)
+
+    def __call__(self, dsn):
+        """Parse a DSN string.
+
+        Return an object with host, port and db attributes.
+        """
+        host, junk, rest = dsn.partition(self.sep)
+        port, junk, db = dsn.partition(self.sep)
+
+        if host:
+            self.host = host
+        if port:
+            self.port = int(port)
+        if db:
+            self.db = int(db)
+
+        return self
+
 class APIHandler(RequestHandler):
     pass
 
@@ -19,8 +47,12 @@ def tsar_server(app):
     server.listen(app.params.port)
     IOLoop.instance().start()
 
-tsar_server.add_param("-P", "--port", default=8000, type=int,
-	help="server port")
+dsn = DSNType(port=6379)
+httpport = 8000
+tsar_server.add_param("-P", "--port", default=httpport, type=int,
+    help="server port (default: %s)" % httpport)
+tsar_server.add_param("-r", "--redis", default=dsn, type=dsn,
+	help="Redis connection host:port/database (default: %s)" % dsn)
 
 if __name__ == "__main__":
     tsar_server.run()
