9fd8ba748fd546dc3e9edc2710bf7b34b5b813f8
Will Maier <willmaier@ml1.net>
2010-03-10 12:28:08 -0600

    Move TypedCSVReader to parsers.

---
+++ lib/tsar/parsers.py
@@ -0,0 +1,17 @@
+from csv import DictReader
+
+class TypedCSVReader(DictReader):
+
+    def __init__(self, f, fieldtypes={}, keytype=str, **kwargs):
+        DictReader.__init__(self, f, **kwargs)
+        self.keytype = str
+        self.fieldtypes = fieldtypes
+    
+    def next(self):
+        d = DictReader.next(self)
+        newd = {}
+        for k in d:
+            coerce = self.fieldtypes.get(k, str)
+            newd[self.keytype(k)] = coerce(d[k])
+
+        return newd
--- lib/tsar/web.py
+++ lib/tsar/web.py
@@ -1,7 +1,5 @@
 import logging
 
-from csv import DictReader
-
 import cli
 
 from redis import Redis
@@ -10,21 +8,7 @@ from tornado.ioloop import IOLoop
 from tornado.web import Application, HTTPError, RequestHandler
 from tornado.wsgi import WSGIApplication
 
-class TypedCSVReader(DictReader):
-
-    def __init__(self, f, fieldtypes={}, keytype=str, **kwargs):
-        DictReader.__init__(self, f, **kwargs)
-        self.keytype = str
-        self.fieldtypes = fieldtypes
-    
-    def next(self):
-        d = DictReader.next(self)
-        newd = {}
-        for k in d:
-            coerce = self.fieldtypes.get(k, str)
-            newd[self.keytype(k)] = coerce(d[k])
-
-        return newd
+from tsar.parsers import TypedCSVReader
 
 class DSNType(object):
     
