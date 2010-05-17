d3753b5c59bb8ec64e0a064cfc4694cfcc7781f7
Will Maier <willmaier@ml1.net>
2010-05-17 09:32:02 -0500

    Add validate and RedisResource tests.

--- tests/test_tsar.py
+++ tests/test_tsar.py
@@ -1,12 +1,18 @@
-from tsar.tsar import validate
+from webob import Request
+from tsar.tsar import RedisResource, validate
 
 from tests import AppTest, BaseTest
+
+class testvalidate(validate):
+
+    def Time(self, value):
+        return super(testvalidate, self).Time(value, now="1272286116.421756")
 
 class TestValidate(BaseTest):
     
     def setUp(self):
         super(TestValidate, self).setUp()
-        self.validate = validate()
+        self.validate = testvalidate()
 
     def test_key(self):
         self.assertEqual(self.validate.Key("foo"), "foo")
@@ -27,6 +33,9 @@ class TestValidate(BaseTest):
 
     def test_number(self):
         self.assertEqual(self.validate.Number("1"), 1)
+
+    def test_number_number(self):
+        self.assertEqual(self.validate.Number(1), 1)
 
     def test_number_big(self):
         self.assertEqual(self.validate.Number("4294967296"), 2**32)
@@ -50,7 +59,51 @@ class TestValidate(BaseTest):
         self.assertEqual(self.validate.Time("1272286116.421756"), 1272286116)
 
     def test_time_relative(self):
-        self.assertEqual(self.validate.Time("-10", 1272286116.421756), 1272286106)
+        self.assertEqual(self.validate.Time("-10"), 1272286106)
 
     def test_time_badinput(self):
         self.assertRaises(ValueError, self.validate.Time, "foo")
+
+    @testvalidate(foo="Key", start="Time", someint="Number", stop="Time")
+    def foo(self, foo, someint, start, stop):
+        self.assertEqual(foo, "bar")
+        self.assertEqual(someint, 10.1)
+        self.assertEqual(start, 1272286116)
+        self.assertEqual(stop, 1272286106)
+
+    @testvalidate(foo="Key")
+    def default_foo(self, foo="baz"):
+        self.assertEqual(foo, "baz")
+
+    def test_validate_plaindict(self):
+        self.foo(foo="bar", someint="10.1", start="1272286116.421756", stop="-10")
+
+    def test_validate_req_params(self):
+        req = Request.blank("/foo?foo=bar&someint=10.1&start=1272286116.421756&stop=-10")
+        self.foo(**req.params)
+
+    def test_validate_default(self):
+        self.default_foo()
+
+    def test_validate_missing(self):
+        self.assertRaises(TypeError, self.foo, someint="10.1",
+            start="1272286116.421756", stop="-10")
+
+    def test_validate_badinput(self):
+        self.assertRaises(TypeError, self.foo, foo="b!ar",
+            someint="10.1", start="1272286116.421756", stop="-10")
+
+class TestRedisResource(BaseTest):
+    
+    def setUp(self):
+        self.resource = RedisResource()
+
+    def test_tokey(self):
+        self.assertEqual(self.resource.tokey("foo", "bar"), "foo!bar")
+
+    def test_fromkey(self):
+        self.assertEqual(self.resource.fromkey("foo!bar"), ["foo", "bar"])
+
+    def test_key_roundtrip(self):
+        input = "foo!bar"
+        self.assertEqual(self.resource.tokey(*self.resource.fromkey(input)), input)
