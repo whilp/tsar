8e7942a4c70b0eb1ecdb6275c6514cb139d633ac
Will Maier <willmaier@ml1.net>
2010-05-17 08:07:38 -0500

    Update coverage script.

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OMIT="tests,docs,/home/will/lib,/home/will/share/neat,/home/will/share/redis-py,setup"
+OMIT="tests,docs,/home/will/share/venv,/home/will/share/neat,/home/will/share/redis-py,setup"
 
 coverage run --branch setup.py test
 coverage report -m --omit $OMIT >| coverage.txt
