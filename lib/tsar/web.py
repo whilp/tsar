971cc82ea2f14cb262b096601081c7db3d40e9bc
Will Maier <willmaier@ml1.net>
2010-07-06 09:39:38 -0500

    Sync with changes in model module.

--- lib/tsar/web.py
+++ lib/tsar/web.py
@@ -2,15 +2,31 @@ import logging
 
 from neat import Resource
 
+from . import model
+from .util import json
+
 def logger(cls): # pragma: nocover
     name = "%s.%s" % (__name__, cls.__class__.__name__)
     return logging.getLogger(name)
 
+# Media types.
+v = "application/vnd.tsar";
+mediatypes = {
+    "records": v + ".records.v1",
+}
+
+model.db = model.connect()
+
 class Records(Resource):
     prefix = "/records/"
+    media = {
+        mediatypes["records"] + "+json": "json",
+        "application/json": "json",
+    }
 
+    # Model interface.
     def record(self, subject, attribute, cf, data):
-        records = Records(subject, attribute, cf)
+        records = model.Records(subject, attribute, cf)
         records.extend(data)
 
     def parseuri(self, uri):
@@ -20,7 +36,12 @@ class Records(Resource):
         """
         return uri.lstrip('/').split('/', 3)[1:]
 
+    # HTTP methods.
     def post(self):
         subject, attribute, cf = self.parseuri(self.req.path_info)
-        self.record(subject, attribute, cf, self.req.content)
+        self.record(subject, attribute, cf, self.req.content["data"])
         self.response.status_int = 204 # No Content
+
+    # HTTP helpers.
+    def handle_json(self):
+        return json.loads(self.req.body)
