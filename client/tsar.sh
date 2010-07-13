d45a651a600c10fa20a9f949239efbde444c9652
Will Maier <willmaier@ml1.net>
2010-07-13 11:20:47 -0500

    Revamp shell client.

--- client/tsar.sh
+++ client/tsar.sh
@@ -1,78 +1,113 @@
 #!/bin/sh
 
-BULK_CONTENT_TYPE=text/csv
-CURL="curl -s"
-OBSERVATION_ENDPOINT=observations
-SERVER=http://tsar.hep.wisc.edu
+TSAR_SERVICE=http://tsar.hep.wisc.edu/records/
+TSAR_MEDIA=application/vnd.tsar.records.v1
+TSAR_AGENT=
+TSAR_CURL="curl -s -A '${TSAR_AGENT}'"
 
-# Record a new observation. 'time' is an integer representing seconds
-# since the Unix Epoch, UTC (ie `date '+%s'`). 'subject' is a string
-# indicating the host or service being monitored (ie "dcache_pfm",
-# "condor_users_${USERNAME}", "g9n01.hep.wisc.edu"). 'attribute' is the
-# property of the subject being monitored ("runtime_seconds", "held_jobs",
-# "free_virtual_memory_kb").
+# Record a new observation. 'timestamp' is an integer representing seconds since
+# the Unix Epoch, UTC (ie `date '+%s'`). 'subject' is a string indicating the
+# host or service being monitored (ie "dcache_pfm", "condor_users_${USERNAME}",
+# "g9n01.hep.wisc.edu"). 'attribute' is the property of the subject being
+# monitored ("runtime_seconds", "held_jobs", "free_virtual_memory_kb"). 'last'
+# is an optional argument and should be a string indicating the name of the
+# function the server should use to consolidate the data ("last", "min", "max",
+# ...).
 #
-# The 'subject' and 'attribute' fields are free-form. By convention,
-# fields with multiple elements in a hierarchy (ie, "dcache" and
-# "pfm") are delimited by underscores.
+# The 'subject' and 'attribute' fields are free-form. By convention, fields with
+# multiple elements in a hierarchy (ie, "dcache" and "pfm") are delimited by
+# underscores.
 #
-# The following request would record a new observation for the 'bar'
-# attribute of the 'foo' subject:
+# The following request would record a new observation for the 'bar' attribute
+# of the 'foo' subject:
 #
 #    tsar_record foo bar $(date '+%s') 10
 #
 tsar_record () {
-    ${CURL} \
-        -d "subject=$1" \
-        -d "attribute=$2" \
-        -d "time=$3" \
-        -d "value=$4" \
-        "${SERVER}/${OBSERVATION_ENDPOINT}"
+    local SUBJECT=$1
+    local ATTRIBUTE=$2
+    local TIMESTAMP=$3
+    local VALUE=$4
+    local CF=${5:-last}
+
+    echo "${TIMESTAMP},${VALUE}" |\
+        tsar_bulk "${SUBJECT}" "${ATTRIBUTE}" "${CF}"
 }
 
-# tsar also supports bulk submission of observations. Bulk records
-# should be formatted in a text file as comma-separated values with column
-# headers. Field values are as defined in tsar_record(). For example:
-#     
-#     time,subject,attribute,value
-#     1268665017,foo,bar,10
-#     1268665027,foo,bar,12
-#     1268665037,foo,bar,11
-#     1268665047,foo,bar,10
+# tsar also supports bulk submission of observations. Bulk records should be
+# formatted as comma-separated values (CSV) with column headers and passed to
+# tsar_bulk on stdin. Two columns, 'timestamp' and 'value', are defined as in
+# tsar_record:
+#
+#     timestamp,value
+#     1268665017,10
+#     1268665027,12
+#     1268665037,11
+#     1268665047,10
 #     [...]
-#     
-# Bulk submissions may include records for more than one (subject,
-# attribute) pair. The CSV text should be sent on standard input.
-# For example:
+#
+# As with tsar_record, the records' ssubject and attribute should be passed as
+# command line arguments. If the desired consolidation function is omitted, a
+# default value will be used.
+#
+# For example (assuming observations.csv contains the above text):
 # 
-#    tsar_bulk < /path/to/observations.csv
+#    tsar_bulk foo bar < /path/to/observations.csv
 #
 tsar_bulk () {
-    ${CURL} -F \
-        "${OBSERVATION_ENDPOINT}=@-;type=${BULK_CONTENT_TYPE}" \
-        "${SERVER}/${OBSERVATION_ENDPOINT}"
+    local SUBJECT=$1
+    local ATTRIBUTE=$2
+    local CF=${3:-last}
+
+    local RESOURCE=${SUBJECT}/${ATTRIBUTE}/${CF}
+    local DATA="timestamp,value\n"
+    while read LINE; do
+
+    (echo "timestamp,value"; while read LINE; do echo $LINE; done) |\
+    #${TSAR_CURL} --request POST --data-binary  --header "Content-Type: ${TSAR_MEDIA}+csv"
+    ${TSAR_CURL} -F \
+        "@-;type=${TSAR_MEDIA}+csv" "${TSAR_SERVICE}/${RESOURCE}"
 }
 
-# Clients may request a range of records for a given (subject,
-# attribute) pair. 'subject' and 'attribute' are strings as defined in
-# tsar_record(). 'start' and 'stop' are either valid 'time' values as
-# defined in tsar_record() or negative integers representing points in
-# time counted back from now. For example, the following request would
-# return all records for (foo, bar):
+# Clients may request a range of records for a given (subject, attribute) pair.
+# 'subject' and 'attribute' are strings as defined in tsar_record(). 'start'
+# and 'stop' are either valid 'time' values as defined in tsar_record() or
+# negative integers representing points in time counted back from now. 'cf' is
+# an optional string as defined in tsar_record(). For example, the following
+# request would return all records for (foo, bar):
+#
 #    tsar_query foo bar 0 -1
 #
+# Successful queries output one or more lines of records, with the 'timestamp'
+# and 'value' fields separated by a space. The output of the above query might
+# look like:
+#
+#    1268665030 10
+#    1268665040 12
+#    1268665050 14
+#
 # Additional options:
-#    sample     (integer)   For each ('subject', 'attribute') pair
-#                           matched by the query, tsar will downsample
-#                           the result set if its length exceeds 'sample'
+#    cf         (string)    Condolidation function.
+#    now        (integer)   Use 'now' when computing relative times.
+#
 tsar_query () {
-    CMD="${CURL} -G"
-    for ARG in subject attribute start stop; do
-        CMD="${CMD} -d ${ARG}=$1"; shift
+    local SUBJECT=$1
+    local ATTRIBUTE=$2
+    local START=$3
+    local STOP=$4
+    local CF=${5:-"last"}
+    local NOW=$6
+
+    local RESOURCE=${SUBJECT}/${ATTRIBUTE}/${CF}
+
+    local CMD="${CURL} -G -d start=${START} -d stop=${STOP}"
+    if [ -n "${NOW}" ]; then
+        CMD="${CMD} -d now=${NOW}"
+    fi
+
+    ${CMD} "${TSAR_SERVICE}/${RESOURCE}" | while read LINE; do
+        TIMESTAMP=${LINE%,*}
+        VALUE=${LINE#*}
+        echo ${TIMESTAMP} ${VALUE}
     done
-    for OPTION in $*; do
-        CMD="${CMD} -d ${OPTION}"
-    done
-    CMD="${CMD} ${SERVER}/${OBSERVATION_ENDPOINT}"
-    ${CMD}
 }
