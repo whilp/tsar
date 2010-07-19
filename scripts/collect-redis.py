a05edb20ed2988b32e625eace592428183afaa09
Will Maier <willmaier@ml1.net>
2010-07-19 14:08:14 -0500

    Re-reorganize.
    --HG--
    rename : lib/tsar/clients/jquery.tsar.js => clients/jquery.tsar.js
    rename : lib/tsar/clients/tsar.sh => clients/tsar.sh
    rename : lib/tsar/clients/tsar.py => lib/tsar/client.py

--- scripts/collect-redis.py
+++ scripts/collect-redis.py
@@ -3,11 +3,11 @@
 import os
 import socket
 import sys
-import subprocess
 import time
 
 import cli
 import tsar
+import redis
 
 now = lambda: int(time.time())
 
@@ -16,51 +16,31 @@ def prepare(data, cfs=["last"]):
         for cf in cfs:
             yield record[:2] + (cf,) + record[2:]
 
-
-class Command(subprocess.Popen):
-    defaults = {
-        "stdout": subprocess.PIPE,
-        "stderr": subprocess.PIPE,
-    }
-    
-    def __init__(self, *args, **kwargs):
-        _kwargs = self.defaults.copy()
-        _kwargs.update(kwargs)
-        super(Command, self).__init__(*args, **kwargs)
-
 @cli.LoggingApp
 def collect_redis(app):
-    rediscmd = "/cms/sw/redis/bin/redis-cli info".split()
-    redis = Command(rediscmd)
+    db = redis.Redis()
 
     name = socket.gethostname() 
-    prefix = "redis_"
-    data = []
-    # key name: converter
-    rediskeys = "used_memory".split()
     statkeys = "size resident share".split()
+    data = []
 
-    pid = None
-    t = now
-    for line in redis.stdout:
-        key, value = line.partition(':')
-        converter = rediskeys.get(key, None)
-        if key is "process_id":
-            pid = int(value)
-        elif key in rediskeys:
-            data.append((name, prefix + key, t, value))
+    t = now()
+    info = db.info()
+    pid = info["process_id"]
+    data.append((name, "redis_used_memory", t, info["used_memory"]))
 
-    if pid is not None:
-        t = int(time.time())
-        statcols = "size resident share text lib data dt".split()
-        with open("/proc/%d/statm" % pid) as stat:
-            statvals = stat.read()
 
-        statdata = dict(zip(statcols, statvals.split()))
-        for key in statkeys:
-            data.append((name, "%sstat_%s" % key, t, value))
+    t = now()
+    statcols = "size resident share text lib data dt".split()
+    with open("/proc/%d/statm" % pid) as stat:
+        statvals = stat.read()
 
-    data = prepare(data, "min max".split())
+    statdata = dict(zip(statcols, statvals.split()))
+    for key in statkeys:
+        data.append((name, "redis_stat_%s" % key, t, statdata[key]))
+
+    data = prepare(data, "min max ave".split())
+    tsar = Tsar("http://g13n01.hep.wisc.edu:8080/records")
     tsar.bulk(data)
 
 if __name__ == "__main__":
