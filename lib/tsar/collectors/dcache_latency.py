729c9f8f7a11f660ee1b168f7fbb0e42031c2b30
Will Maier <willmaier@ml1.net>
2010-07-21 09:34:35 -0500

    Fix conflicting option.

--- lib/tsar/collectors/dcache_latency.py
+++ lib/tsar/collectors/dcache_latency.py
@@ -69,7 +69,7 @@ default_srm = "srm://cmssrm.hep.wisc.edu:8443/srm/managerv2?SFN="
 default_dcap = "dcap://cmsdcap.hep.wisc.edu:22125"
 default_gsiftp = "gsiftp://cmsgridftp.hep.wisc.edu:2811"
 
-dcache_latency.add_param("-t", "--test-file", default=default_testfile,
+dcache_latency.add_param("-T", "--test-file", default=default_testfile,
     help="Remote test file (default: %s)" % default_testfile)
 dcache_latency.add_param("-d", "--destination", default=default_destination,
     help="Destination directory (default: %s)" % default_destination)
