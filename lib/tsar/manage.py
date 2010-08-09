0a923ac114598d6899ece4dc0ce4002a260faa26
Will Maier <wcmaier@hep.wisc.edu>
2010-08-09 10:14:56 -0500

    Wrap around subcommands.

--- lib/tsar/manage.py
+++ lib/tsar/manage.py
@@ -1,8 +1,14 @@
 import cli
-
+    
 @cli.LoggingApp
 def manage(app):
-    pass
+    cmd = app.commands[app.params.command]
+    return cmd()
+
+manage.commands = {
+}
+
+manage.add_param("command", choices=manage.commands, help="sub command")
 
 if __name__ == "__main__":
     manage.run()
