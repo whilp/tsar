13da9d0ebbe074346298435345ca1a8898b501de
Will Maier <wcmaier@hep.wisc.edu>
2010-08-09 10:10:49 -0500

    Mostly update shell client.

--- clients/tsar.sh
+++ clients/tsar.sh
@@ -10,22 +10,27 @@ SERVER=http://tsar.hep.wisc.edu
 # indicating the host or service being monitored (ie "dcache_pfm",
 # "condor_users_${USERNAME}", "g9n01.hep.wisc.edu"). 'attribute' is the
 # property of the subject being monitored ("runtime_seconds", "held_jobs",
-# "free_virtual_memory_kb").
+# "free_virtual_memory_kb"). 'cf' is a string indicating the consolidation
+# function the server should use when aggregating data for this series.
 #
 # The 'subject' and 'attribute' fields are free-form. By convention,
 # fields with multiple elements in a hierarchy (ie, "dcache" and
-# "pfm") are delimited by underscores.
+# "pfm") are delimited by underscores. 'cf' must match a function implemented
+# by the server
 #
 # The following request would record a new observation for the 'bar'
-# attribute of the 'foo' subject:
+# attribute of the 'foo' subject using the 'max' consolidation function:
 #
-#    tsar_record foo bar $(date '+%s') 10
+#    tsar_record foo bar max $(date '+%s') 10
 #
 tsar_record () {
     ${CURL} \
         -d "subject=$1" \
         -d "attribute=$2" \
-        -d "time=$3" \
-        -d "value=$4" \
-        "${SERVER}/${OBSERVATION_ENDPOINT}"
+        -d "attribute=$3" \
+        -d "time=$4" \
+        -d "value=$5" \
+        "${SERVICE}"
 }
+
+# tsar_record "condor_users_${USER_NAME}" held_jobs last "${DATE}" "${HELD_JOBS}"
