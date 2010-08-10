207f96b1b6d60497c1e9d510ce52b238b4f8d5ca
Will Maier <wcmaier@hep.wisc.edu>
2010-08-10 07:50:10 -0500

    No need to overwrite __init__().

--- lib/tsar/tsar.py
+++ lib/tsar/tsar.py
@@ -2,16 +2,13 @@ from .commands import Command
 
 class Tsar(Command):
     name = "tsar"
-
-    def __init__(self, *args, **kwargs):
-        super(Tsar, self).__init__(*args, **kwargs)
-        self.setup()
     
     @staticmethod
     def main(self):
         pass
 
 tsar = Tsar()
+tsar.setup()
 
 if __name__ == "__main__":
     tsar.run()
