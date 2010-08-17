fd88e4c652f87fa3dda753030d637fefe7985db5
Will Maier <wcmaier@hep.wisc.edu>
2010-08-17 11:50:30 -0500

    Tweak replace/insert, add appendkey.

--- lib/tsar/collectors/helpers.py
+++ lib/tsar/collectors/helpers.py
@@ -3,9 +3,16 @@ import operator
 
 from io import StringIO
 
-insert = lambda l, i, o: l[0:i] + [o] + l[i:]
-replace = lambda l, i, o: l[0:i] + [o] + l[i + 1:]
+def replace(seq, i, other, width=None):
+    if not isinstance(other, list):
+        other = [other]
+    if width is None:
+        width = len(other)
+    return seq[0:i] + other + seq[i + width:]
+insert = functools.partial(replace, width=0)
+
 incrkey = lambda d, k, i=1: operator.setitem(d, k, d.setdefault(k, 0) + i)
+appendkey = lambda d, k, v: operator.setitem(d, k, d.setdefault(k, []) + [v])
 median = lambda x: sorted(x)[len(x)/2]
 
 def trim(s, subs, reverse=False):
